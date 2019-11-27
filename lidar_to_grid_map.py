remove gridmap, add shortest path algorithm, fixed calculation

lidar_to_grid_map.py
@@ -6,11 +6,12 @@ author: Erno Horvath, Csaba Hajdu based on Atsushi Sakai's scripts (@Atsushi_twi
 
 """
 
-import cv2
+# import cv2
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
+import a_star
 
 EXTEND_AREA = 1.0
 
@@ -18,6 +19,11 @@ EXTEND_AREA = 1.0
 def lidar_file_read(lidar):
     """
     Reading LIDAR laser beams (angles and corresponding distance data)
+    input: 
+        lidar: path to lidar file
+    output:
+        angles: lidar angle data
+        distance: lidar distance data
     """
     measures = [line.split(",") for line in open(lidar)]
     scanId = 0
@@ -46,6 +52,11 @@ def lidar_file_read(lidar):
 def flight_file_read(flight):
     """
     Reading flight path file
+    input:
+        flight: path to flight pathfile
+    output:
+        x: drone x position
+        y: drone y position
     """
     points = [line.split(",") for line in open(flight)]
     scanId = 0
@@ -65,240 +76,107 @@ def flight_file_read(flight):
     y = np.array(y)
     return x, y
 
-
-def bresenham(start, end):
+def write_new_flight_path(rx, ry):
     """
-    Implementation of Bresenham's line drawing algorithm
-    See en.wikipedia.org/wiki/Bresenham's_line_algorithm
-    Bresenham's Line Algorithm
-    Produces a np.array from start and end (original from roguebasin.com)
-    >>> points1 = bresenham((4, 4), (6, 10))
-    >>> print(points1)
-    np.array([[4,4], [4,5], [5,6], [5,7], [5,8], [6,9], [6,10]])
+    write new flight path to newFlightPath.csv
+    input:
+        rx: drone new x position
+        ry: drone new y position
+    output:
+        newFlightPath.csv: drone new flight path file
     """
-    # setup initial conditions
-    x1, y1 = start
-    x2, y2 = end
-    dx = x2 - x1
-    dy = y2 - y1
-    is_steep = abs(dy) > abs(dx) # determine how steep the line is
-    if is_steep: # rotate line
-        x1, y1 = y1, x1
-        x2, y2 = y2, x2
-    swapped = False # swap start and end points if necessary and store swap state
-    if x1 > x2:
-        x1, x2 = x2, x1
-        y1, y2 = y2, y1
-        swapped = True
-    dx = x2 - x1 # recalculate differentials
-    dy = y2 - y1 # recalculate differentials
-    error = int(dx / 2.0) # calculate error
-    ystep = 1 if y1 < y2 else -1
-    # iterate over bounding box generating points between start and end
-    y = y1
-    points = []
-    for x in range(x1, x2 + 1):
-        coord = [y, x] if is_steep else (x, y)
-        points.append(coord)   
-        error -= abs(dy)
-        if error < 0:
-            y += ystep
-            error += dx
-    if swapped: # reverse the list if the coordinates were swapped
-        points.reverse()
-    points = np.array(points)
-    return points
+    f= open("newFlightPath.csv", "w+")
+    i = 0
+    for (x,y) in zip(rx,ry):
+        scanid = "%d,1\n\r" % i
+        data = "%5.5f,%5.5f\n\r" % (float(x), float(y))
+        f.write(scanid)
+        f.write(data)
+        i += 1
+    f.close()
+    print("newFlightPath.csv is saved")
 
-
-def calc_grid_map_config(ox, oy, xyreso):
+def mapping(ang, dist, dronex, droney):
     """
-    Calculates the size, and the maximum distances according to the the measurement center
+    collecting lidar projection data and plot
+    input:
+        ang: lidar angle data(deg)
+        dist; lidar distance data per angle (mm)
+        dronex: drone x position
+        droney: drone y position
+    output:
+        tx: lidar x projection data
+        ty: lidar y projection data
     """
-    minx = round(min(ox) - EXTEND_AREA / 2.0)
-    miny = round(min(oy) - EXTEND_AREA / 2.0)
-    maxx = round(max(ox) + EXTEND_AREA / 2.0)
-    maxy = round(max(oy) + EXTEND_AREA / 2.0)
-    xw = int(round((maxx - minx) / xyreso))
-    yw = int(round((maxy - miny) / xyreso))
-    print("The grid map is ", xw, "x", yw, ".")
-    return minx, miny, maxx, maxy, xw, yw
-
-
-def atan_zero_to_twopi(y, x):
-    angle = math.atan2(y, x)
-    if angle < 0.0:
-        angle += math.pi * 2.0
-    return angle
-
-
-def init_floodfill(cpoint,  opoints, xypoints, mincoord, xyreso):
-    """
-    cpoint: center point
-    opoints: detected obstacles points (x,y)
-    xypoints: (x,y) point pairs
-    """
-    centix, centiy = cpoint
-    prev_ix, prev_iy = centix - 1, centiy
-    ox, oy = opoints
-    xw, yw = xypoints
-    minx, miny = mincoord
-    pmap = (np.ones((xw, yw))) * 0.5
-    for (x, y) in zip(ox, oy):
-        ix = int(round((x - minx) / xyreso))  # x coordinate of the the occupied area
-        iy = int(round((y - miny) / xyreso))  # y coordinate of the the occupied area
-        free_area = bresenham((prev_ix, prev_iy), (ix, iy))
-        for fa in free_area:
-            pmap[fa[0]][fa[1]] = 0  # free area 0.0
-        prev_ix = ix
-        prev_iy = iy
-    return pmap
-
-
-def flood_fill(cpoint, pmap):
-    """
-    cpoint: starting point (x,y) of fill
-    pmap: occupancy map generated from Bresenham ray-tracing
-    """
-    # Fill empty areas with queue method
-    sx, sy = pmap.shape
-    fringe = deque()
-    fringe.appendleft(cpoint)
-    while fringe:
-        n = fringe.pop()
-        nx, ny = n
-        # West
-        if nx > 0:
-            if pmap[nx - 1, ny] == 0.5:
-                pmap[nx - 1, ny] = 0.0
-                fringe.appendleft((nx - 1, ny))
-        # East
-        if nx < sx - 1:
-            if pmap[nx + 1, ny] == 0.5:
-                pmap[nx + 1, ny] = 0.0
-                fringe.appendleft((nx + 1, ny))
-        # North
-        if ny > 0:
-            if pmap[nx, ny - 1] == 0.5:
-                pmap[nx, ny - 1] = 0.0
-                fringe.appendleft((nx, ny - 1))
-        # South
-        if ny < sy - 1:
-            if pmap[nx, ny + 1] == 0.5:
-                pmap[nx, ny + 1] = 0.0
-                fringe.appendleft((nx, ny + 1))
-
-
-def generate_ray_casting_grid_map(dx, dy, tx, ty, ang, dist, xyreso, breshen=True):
-    """
-    The breshen boolean tells if it's computed with bresenham ray casting (True) or with flood fill (False)
-    """
-    minx, miny, maxx, maxy, xw, yw = calc_grid_map_config(tx, ty, xyreso)
-    pmap = np.ones((xw, yw))/2 # default 0.5 -- [[0.5 for i in range(yw)] for i in range(xw)] 
-    counter = 0
-    for (cx, cy) in zip(dx, dy):
-        angle = np.array(ang[counter])
-        distance = np.array(dist[counter])
-        ox = cx + (np.cos(angle * np.pi / 180.) * distance)
-        oy = cy - (np.sin(angle * np.pi / 180.) * distance)
-        counter += 1
-        centix = int(math.ceil((cx - minx) / xyreso)) # center x coordinate of the grid map
-        centiy = int(math.ceil((cy - miny) / xyreso)) # center y coordinate of the grid map
-        print("center :", centix, ",", centiy)
-        print("min :", minx, ",", miny)
-        # occupancy grid computed with bresenham ray casting
-        if breshen:
-            for (x, y) in zip(ox, oy):
-                ix = int(math.ceil((x - minx) / xyreso)) # x coordinate of the the occupied area
-                iy = int(math.ceil((y - miny) / xyreso)) # y coordinate of the the occupied area
-                laser_beams = bresenham((centix, centiy), (ix, iy)) # line form the lidar to the cooupied point
-                for laser_beam in laser_beams:
-                    pmap[laser_beam[0]][laser_beam[1]] = 0.0 # free area 0.0
-                pmap[ix][iy] = 1.0     # occupied area 1.0
-                pmap[ix+1][iy] = 1.0   # extend the occupied area
-                pmap[ix][iy+1] = 1.0   # extend the occupied area
-                pmap[ix+1][iy+1] = 1.0 # extend the occupied area
-        # occupancy grid computed with with flood fill
-        else:
-            pmap = init_floodfill((centix, centiy), (ox, oy), (xw, yw),  (minx, miny), xyreso)
-            flood_fill((centix, centiy), pmap)
-            pmap = np.array(pmap, dtype=np.float)
-            for (x, y) in zip(ox, oy):
-                ix = int(round((x - minx) / xyreso))
-                iy = int(round((y - miny) / xyreso))
-                pmap[ix][iy] = 1.0     # occupied area 1.0
-                pmap[ix+1][iy] = 1.0   # extend the occupied area
-                pmap[ix][iy+1] = 1.0   # extend the occupied area
-                pmap[ix+1][iy+1] = 1.0 # extend the occupied area
-    return pmap, minx, maxx, miny, maxy, xyreso
-
-def generate_line(pmap):
-    src = np.uint8(255 - (pmap * 255))
-    dst = cv2.Canny(src, 50, 150, 3)
-    cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
-
-    lines = cv2.HoughLines(dst, 1, np.pi / 180, 150, None, 0, 0)
-    points = []
-    for i in range(0, len(lines)):
-        rho = lines[i][0][0]
-        theta = lines[i][0][1]
-        a = math.cos(theta)
-        b = math.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-        pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-        cv2.line(cdst, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
-        # line = [x0 + 1000*(-b), y0 + 1000*a, x0 - 1000 * b, y0 - 1000 * a]
-    cv2.imshow("source", src)
-    cv2.imshow("detection standard hough line", cdst)
-    return lines
-
-def main():
-    """
-    Example usage
-    """
-    print(__file__, "start")
-    xyreso = 0.1 # x-y grid resolution
-    ang, dist = lidar_file_read("LIDARPoints.csv")
-    dronex, droney = flight_file_read("FlightPath.csv")
     tx = []
     ty = []
     counter = 0
-    for (x, y) in zip(dronex, droney):
-        for (t, d) in zip(ang, dist):
-            angle = np.array(t)
-            distance = np.array(d)
-            tx = np.append(tx, (x + (np.cos(angle * np.pi / 180.) * distance)))
-            ty = np.append(ty, (y - (np.sin(angle * np.pi / 180.) * distance)))
-    pmap, minx, maxx, miny, maxy, xyreso = generate_ray_casting_grid_map(dronex, droney, tx, ty, ang, dist, xyreso, True)
-    lines = generate_line(pmap)
     for (cx, cy) in zip(dronex, droney):
         angle = np.array(ang[counter])
         distance = np.array(dist[counter])
-        ox = cx + (np.cos(angle * np.pi / 180.) * distance)
-        oy = cy - (np.sin(angle * np.pi / 180.) * distance)
+        dotx = []
+        doty = []
+        for (d,a) in zip(distance, angle):
+            if d != 0:
+                dotx = np.append(dotx, cx + (math.cos(a * math.pi / 180.) * d))
+                doty = np.append(doty, cy - (math.sin(a * math.pi / 180.) * d))
+        tx = np.append(tx, dotx)
+        ty = np.append(ty, doty)
         counter += 1
-        xyres = np.array(pmap).shape
-        plt.figure(1, figsize=(15,8))
-        plt.subplot(122)
+        plt.figure(1, figsize=(8,8))
         plt.cla()
-        plt.imshow(pmap, cmap="PiYG_r") # cmap = "binary" "PiYG_r" "PiYG_r" "bone" "bone_r" "RdYlGn_r"
-        plt.clim(-0.4, 1.4)
-        plt.gca().set_xticks(np.arange(-.5, xyres[1], 1), minor=True)
-        plt.gca().set_yticks(np.arange(-.5, xyres[0], 1), minor=True)
-        plt.gca().invert_yaxis()
-        plt.grid(True, which="minor", color="w", linewidth=0.6, alpha=0.5)
-        plt.subplot(121)
-        plt.cla()
-        plt.plot([oy,np.full(np.size(oy), cy)], [ox,np.full(np.size(ox), cx)], "ro-")
+        plt.plot(tx, ty, "ro")
         plt.axis("auto")
-        plt.plot(cy,cx, "ob")
+        plt.plot(cx,cy, "ob")
         plt.gca().set_aspect("equal", "box")
         plt.gca().invert_yaxis()
         bottom, top = plt.ylim()  # return the current ylim
         plt.ylim((top, bottom)) # rescale y axis, to match the grid orientation
         plt.grid(True)
-        plt.pause(0.5)
+        plt.pause(0.01)
+    return tx, ty
+
+def path_finding(tx, ty, dronex, droney):
+    """
+    A star path finding and plot new route
+    input;
+        tx: lidar projection x coordinat [m]
+        ty: lidar projection y coordinat [m]
+        dronex: drone position x coordinate [m]
+        droney: drone position y coordinate [m]
+    output:
+        rx: new x position path
+        rx: new y position path
+    """
+    sx = dronex[0]
+    sy = droney[0]
+    gx = dronex[-1]
+    gy = droney[-1]
+    plt.figure(1, figsize=(8,8))
+    plt.cla()
+    plt.plot(tx, ty, ".k")
+    plt.plot(sx, sy, "og")
+    plt.plot(gx, gy, "xb")
+    plt.grid(True)
+    plt.axis("equal")
+
+    astar = a_star.AStarPlanner(tx, ty, 0.3, 0.3)
+    rx, ry = astar.planning(sx,sy,gx,gy)
+    plt.plot(rx, ry, "-r")
+
+    return rx, ry
+
+
+def main():
+    print(__file__, "start")
+    
+    ang, dist = lidar_file_read("LIDARPoints.csv")
+    dronex, droney = flight_file_read("FlightPath.csv")
+
+    tx, ty = mapping(ang, dist, dronex, droney)
+    rx, ry = path_finding(tx, ty, dronex, droney)
+    write_new_flight_path(rx, ry)
+
     plt.show()
 
 if __name__ == '__main__':